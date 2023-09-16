441e9e4347853e390140bc4d17ef54ca9881dd49
@mario.krok-adamski <mario.krok-adamski@olx.pl>
2023-09-16 21:13:38 +0200

    setup server

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,28 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require("express");
+const port = 3000;
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded());
+app.use(express.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+app.get("/", (req, res) => {
+  res.send(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
